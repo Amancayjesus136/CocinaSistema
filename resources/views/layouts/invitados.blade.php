73d195c1fb885fc4a49c9e70a818464445057417
Amancayjesus136 <amancayjesus136@gmail.com>
2023-09-27 15:42:02 -0500

    gradient-2

--- resources/views/layouts/invitados.blade.php
+++ resources/views/layouts/invitados.blade.php
@@ -46,7 +46,7 @@
                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                             <div>
                                 <a href="index.html" class="d-inline-block auth-logo">
-                                    <img src="assets/images/logo-light.png" alt="" height="150">
+                                    <!-- <img src="assets/images/logo-light.png" alt="" height="150"> -->
                                 </a>
                             </div>
                             <p class="mt-3 fs-15 fw-medium">Phenomenal - Sistema de Placas</p>
--- resources/views/layouts/menu-sidebar.blade.php
+++ resources/views/layouts/menu-sidebar.blade.php
@@ -126,19 +126,19 @@
                     <!-- Dark Logo-->
                     <a href="{{ route('login') }}" class="logo logo-dark">
                         <span class="logo-sm">
-                            <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                            <!-- <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22"> -->
                         </span>
                         <span class="logo-lg">
-                            <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="50" style="margin-top: 20px;">
+                            <!-- <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="50" style="margin-top: 20px;"> -->
                         </span>
                     </a>
                     <!-- Light Logo-->
                     <a href="{{ route('login') }}" class="logo logo-light">
                         <span class="logo-sm">
-                            <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                            <!-- <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22"> -->
                         </span>
                         <span class="logo-lg">
-                        <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="100" style="margin-top: 15px;">
+                        <!-- <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="100" style="margin-top: 15px;"> -->
                         </span>
                     </a>
                     <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
